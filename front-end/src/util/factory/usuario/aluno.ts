eb527464aebea49f864e919e89a744607fb8d69e
Matheus Marçal <mtsconde@hotmail.com>
2017-10-25 00:43:37 -0200

    adicionado lista de categoria profissional para Aluno

--- front-end/src/util/factory/usuario/aluno.ts
+++ front-end/src/util/factory/usuario/aluno.ts
@@ -1,6 +1,8 @@
 import { UsuarioInfo } from '../usuario/usuario-info';
+import { CategoriaProfissional } from '../categoria-profissional/categoria-profissional';
 
 export class Aluno {
     id?: string;
     usuarioInfo: UsuarioInfo;
+    categoriaprossional: Array<CategoriaProfissional>;
 }
