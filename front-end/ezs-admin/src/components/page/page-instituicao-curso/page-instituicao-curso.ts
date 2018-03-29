70965d64dcc17e81d45b11ffb0f655d068abaa5a
Matheus Carvalho <mtw.nda@hotmail.com>
2018-03-29 00:33:29 -0300

    [master] - corrigir identação.

--- front-end/ezs-admin/src/components/page/page-instituicao-curso/page-instituicao-curso.ts
+++ front-end/ezs-admin/src/components/page/page-instituicao-curso/page-instituicao-curso.ts
@@ -117,7 +117,7 @@ export class PageInstituicaoCursoComponent extends Vue {
         });
     }
 
-    async onCursoGradeChanged(cursoGrade){
+    async onCursoGradeChanged(cursoGrade) {
         this.$forceUpdate();
     }
 
